e78d4bafc0676d3beb45231a10393a9568ba42f8
eric <ericinchina886@gmail.com>
2025-08-07 18:29:05 +0900

    update

--- src/kidou/release.ts
+++ src/kidou/release.ts
@@ -5,10 +5,22 @@ const note_0_0_15 = `
 - Support Claude Opus 4.1
 - Support Claude code plan mode
 `;
+
+const note_0_0_16 = `
+- Improve user interface
+- Support slash command in chat
+`;
+
+
 export const releaseInfo = {
     "0.0.15": {
         "releaseDate": "2025-08-06",
         "releaseNotes": note_0_0_15,
+        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
+    },
+    "0.0.16": {
+        "releaseDate": "2025-08-07",
+        "releaseNotes": note_0_0_16,
         "releaseUrl": "https://embodyai.co.jp/kidou/releases",
     },
 }

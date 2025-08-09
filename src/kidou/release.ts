4c920ccbdcf6165261b59a8f03484053cde24350
eric <ericinchina886@gmail.com>
2025-08-09 22:20:22 +0900

    update

--- src/kidou/release.ts
+++ src/kidou/release.ts
@@ -10,6 +10,12 @@ const note_0_0_18 = `
 - Support Claude Opus 4.1
 `;
 
+const note_0_0_19 = `
+- Support Cursor Agent cli
+- Support set default provider in settings
+- UI improvements
+- Support auto-commit in session
+`;
 
 export const releaseInfo = {
     "0.0.17": {
@@ -20,6 +26,11 @@ export const releaseInfo = {
     "0.0.18": {
         "releaseDate": "2025-08-08",
         "releaseNotes": note_0_0_18,
+        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
+    },
+    "0.0.19": {
+        "releaseDate": "2025-08-09",
+        "releaseNotes": note_0_0_19,
         "releaseUrl": "https://embodyai.co.jp/kidou/releases",
     },
 }

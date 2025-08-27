84d92b8027711baa1e2f4372f2561ba862e4bcbf
eric <ericinchina886@gmail.com>
2025-08-27 23:27:36 +0900

    update

--- src/kidou/release.ts
+++ src/kidou/release.ts
@@ -1,31 +1,8 @@
-
-
-const note_0_0_17 = `
-- Improve user interface
-- Support slash command in chat
-`;
-
-const note_0_0_18 = `
+const note_0_0_23 = `
+Added:
 - Support claude code sub agents
 - Support Claude Opus 4.1
-`;
-
-const note_0_0_19 = `
-- Support Cursor Agent cli
-- Support set default provider in settings
-- UI improvements
-- Support auto-commit in session
-`;
-
-const note_0_0_20 = `
-- Support multiple terminal tabs in one session
-- Support selection in terminal
-- UI improvements
-- Support set a session name when creating a new session
-`;
-
-const note_0_0_21 = `
-Added:
+- Supports to scroll horizontally when view diffs
 - Support view git diff by file
 - Full screen mode for Changes tab
 
@@ -34,50 +11,13 @@ Fixed:
 - Bad performance when making a big change
 
 Development:
-- Kidou cloud is in development
-`;
-
-const note_0_0_22 = `
-Added:
-- Supports to scroll horizontally when view diffs
-
-Fixed:
-- Fixed a bug that show wrong commit message when rebase to main
-- Performance improvements
-
-Development:
-- Kidou cloud is in development
+- Better diff view
 `;
 
 export const releaseInfo = {
-    "0.0.17": {
-        "releaseDate": "2025-08-07",
-        "releaseNotes": note_0_0_17,
-        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
-    },
-    "0.0.18": {
-        "releaseDate": "2025-08-08",
-        "releaseNotes": note_0_0_18,
-        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
-    },
-    "0.0.19": {
-        "releaseDate": "2025-08-09",
-        "releaseNotes": note_0_0_19,
-        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
-    },
-    "0.0.20": {
-        "releaseDate": "2025-08-11",
-        "releaseNotes": note_0_0_20,
-        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
-    },
-    "0.0.21": {
-        "releaseDate": "2025-08-13",
-        "releaseNotes": note_0_0_21,
-        "releaseUrl": "https://embodyai.co.jp/kidou/releases",
-    },
-    "0.0.22": {
-        "releaseDate": "2025-08-15",
-        "releaseNotes": note_0_0_22,
+    "0.0.23": {
+        "releaseDate": "2025-08-27",
+        "releaseNotes": note_0_0_23,
         "releaseUrl": "https://embodyai.co.jp/kidou/releases",
     },
 }
